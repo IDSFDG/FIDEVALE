2238bd2782f4dea83d56118ce4ce78206eb25a13
IDSFDG <fidegarzag.ids@gmail.com>
2025-06-26 15:07:49 -0600

    Add files via upload

--- NeniVents.js
+++ NeniVents.js
@@ -63319,12 +63319,12 @@ rtl.module("uCargarConsultas",["System","SysUtils","Classes","JS","Web","WEBLib.
     
     
        table.on("tableBuilt", function(){
-          table.setLocale("espaniol"); //set locale to espaniol
-           table.activeSheet("uno"); //make the info sheet active
-           table.setSheetData("uno",sheetDataConsulta);       // API
+         // table.setLocale("espaniol"); //set locale to espaniol
+        //   table.activeSheet("uno"); //make the info sheet active
+        //   table.setSheetData("uno",sheetDataConsulta);       // API
            var cols = table.getColumns() //get array of column components
     
-            cols[0].updateDefinition({title:'',width:8});
+            cols[0].updateDefinition({title:' ',width:8});
             cols[1].updateDefinition({title:'Nombre',width:150,responsive:0});
             cols[2].updateDefinition({title:'Articulo',width:150,responsive:0});
             cols[3].updateDefinition({title:'Importe',width:75,responsive:0});
@@ -64823,7 +64823,7 @@ rtl.module("Unit1",["System","SysUtils","Classes","JS","Web","WEBLib.Graphics","
           table.clearSheet("uno"); //clear the data from the info sheet
           var cols = table.getColumns() //get array of column components
       
-              cols[0].updateDefinition({title:'',width:8});
+              cols[0].updateDefinition({title:' ',width:8});
               cols[1].updateDefinition({title:'Nombre',width:150,responsive:0});
               cols[2].updateDefinition({title:'Articulo',width:150,responsive:0});
               cols[3].updateDefinition({title:'Importe',width:75,responsive:0});
