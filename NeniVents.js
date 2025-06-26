05d3552a245b1ff03a4cc5dac5b49a8f915edea3
IDSFDG <fidegarzag.ids@gmail.com>
2025-06-25 22:24:34 -0600

    Add files via upload

--- NeniVents.js
+++ NeniVents.js
@@ -63316,11 +63316,12 @@ rtl.module("uCargarConsultas",["System","SysUtils","Classes","JS","Web","WEBLib.
            table.setSheetData("uno",sheetDataConsulta);       // API
            var cols = table.getColumns() //get array of column components
     
-            cols[1].updateDefinition({title:'Nombre',width:250});
-            cols[2].updateDefinition({title:'Articulo',width:250});
-            cols[3].updateDefinition({title:'Importe',width:150});
-            cols[4].updateDefinition({title:'Pagad/Entreg',width:150});
-            cols[5].updateDefinition({title:'VD/Sub',width:150});
+            cols[0].updateDefinition({title:'',width:0});
+            cols[1].updateDefinition({title:'Nombre',width:150});
+            cols[2].updateDefinition({title:'Articulo',width:150});
+            cols[3].updateDefinition({title:'Importe',width:75});
+            cols[4].updateDefinition({title:'Pag./Entr.',width:80});
+            cols[5].updateDefinition({title:'VD/SUB',width:70});
         //  alert('tableBuilt');
     
     
